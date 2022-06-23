e6276da0030c7195932d2cd93a7ce0f75ad01245
Buckhalton <isaiah.buckhalton@gmail.com>
2022-06-23 00:02:10 -0500

    adds useEffect to query api, basic map of data

--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -1,6 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function BreweryList() {
+  const [breweries, setBreweries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBreweries = async () => {
+      try{
+        const response = await fetch('https://api.openbrewerydb.org/breweries?per_page=10');
+        const json = await response.json()
+        setBreweries(json);
+        setIsLoading(false);
+      } catch(err) {
+        setIsLoading(false);
+        throw Error('Unable to fetch breweries:', err);
+      }
+    }
+    fetchBreweries();
+  }, [])
+
   return (
     <main>
       <h1>Brewery Catalog</h1>
@@ -10,9 +29,15 @@ export default function BreweryList() {
         <button type='reset'>Reset</button>
       </form>
       <ul>
-        <li>
-          <Link to='/breweries/1'>Brewery 1</Link> - Brewtown, OR
-        </li>
+
+      {isLoading ? 'Spinner and/or skeleton goes here' : breweries.map(brewery => {
+        return (
+          <li>
+            <Link to={`/breweries/${brewery.id}`}>{brewery.name}</Link>
+            <p>{`${brewery.city}, ${brewery.state}`}</p>
+          </li>
+        )
+      })}
       </ul>
     </main>
   );
